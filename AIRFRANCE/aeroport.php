5f7c4e1f330d9aab245b062c6838d3a58b4a31a7
ryad1602 <117727201+ryad1602@users.noreply.github.com>
2022-11-08 17:59:22 +0100

    Update aeroport.php

--- AIRFRANCE/aeroport.php
+++ AIRFRANCE/aeroport.php
@@ -1,1 +1,36 @@
 <h2>Gestion des aeroports</h2>
+
+<?php 
+
+$laeroport = null;
+
+	if(isset($_GET['action']) && isset($_GET['idaeroport'])){
+		$action = $_GET['action'];
+		$idaeroport = $_GET['idaeroport'];
+		switch($action){
+			case "sup" : $unControleur->deleteAeroports($idaeroport) ; break;
+			case "edit" : $laeroport = $unControleur->selectWhereAeroports($idaeroport);
+				
+				break;
+		}
+	}
+
+
+
+	require_once("vue/vue_insert_aeroport.php");
+	if(isset($_POST['Valider']))
+	{
+		$unControleur->insertAeroports($_POST);
+	}
+
+	if(isset($_POST['Filtrer']))
+	{
+		$mot = $_POST['mot'];
+		$lesAeroports = $unControleur->selectLikeAeroports($mot);
+	}else{
+		$lesAeroports = $unControleur->selectAllAeroports();
+	}
+
+	require_once("vue/vue_les_aeroports.php");
+
+?>
