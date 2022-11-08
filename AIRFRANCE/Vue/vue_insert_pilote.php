5c92d08d8d4b136d6d7a2ab45d6232bb8da8322f
ryad1602 <117727201+ryad1602@users.noreply.github.com>
2022-11-08 13:10:22 +0100

    Update vue_insert_pilote.php

--- AIRFRANCE/Vue/vue_insert_pilote.php
+++ AIRFRANCE/Vue/vue_insert_pilote.php
@@ -3,20 +3,28 @@
 	<table>
 		<tr>
 			<td>Nom du pilote</td>
-			<td><input type="text" name="nom"></td>
+			<td><input type="text" name="nom" value="<?= ($lePilote !=null)?$lePilote['nom']:'' ?>"></td>
 		</tr>
 		<tr>
-			<td>Prenom du pilote</td>
-			<td><input type="text" name="prenom"></td>
+			<td>Prénom du pilote</td>
+			<td><input type="text" name="prenom"
+				value="<?= ($lePilote !=null)?$lePilote['prenom']:'' ?>"></td>
 		</tr>
 		<tr>
-			<td>anciennete du pilote</td>
-			<td><input type="text" name="anciennete"></td>
+			<td>Anciennete du pilote</td>
+			<td><input type="text" name="anciennete"
+				value="<?= ($lePilote !=null)?$lePilote['anciennete']:'' ?>"></td>
 		</tr>
 		<tr>
 			<td><input type="reset" name="Annuler" value="Annuler"></td>
-			<td><input type="submit" name="Valider" value="Valider"></td>
+			<td><input type="submit" 
+				<?= ($lePilote !=null)? 'name="Modifier" value="Modifier" ' : 'name="Valider" value="Valider" ' ?>
+				name="Valider" value="Valider"
+
+				></td>
 		</tr>
+		<input type="hidden" name="idpilote" value="
+		<?= ($lePilote !=null)? $lePilote['idpilote'] : '' ?>">
 	</table>
 	
 </form>
