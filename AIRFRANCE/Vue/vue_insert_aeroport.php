ac6dc9402eddd875e750be537c58aa280a4cd1a2
ryad1602 <117727201+ryad1602@users.noreply.github.com>
2022-11-08 18:11:34 +0100

    Update vue_insert_aeroport.php

--- AIRFRANCE/Vue/vue_insert_aeroport.php
+++ AIRFRANCE/Vue/vue_insert_aeroport.php
@@ -1,1 +1,31 @@
+<h3>Ajout d'un aeroport</h3>
+<form method="post">
+	<table>
+		<tr>
+			<td>Nom de l'aeroport</td>
+			<td><input type="text" name="nom" value="<?= ($laeroport !=null)?$laeroport['nom']:'' ?>"></td>
+		</tr>
+		<tr>
+			<td>Pays de l'aeroport</td>
+			<td><input type="text" name="prenom"
+				value="<?= ($laeroport !=null)?$laeroport['pays']:'' ?>"></td>
+		</tr>
+		<tr>
+			<td>ville de l'aeroport</td>
+			<td><input type="text" name="ville"
+				value="<?= ($laeroport !=null)?$laeroport['ville']:'' ?>"></td>
+		</tr>
+		<tr>
+			<td><input type="reset" name="Annuler" value="Annuler"></td>
+			<td><input type="submit" 
+				<?= ($laeroport !=null)? 'name="Modifier" value="Modifier" ' : 'name="Valider" value="Valider" ' ?>
+				name="Valider" value="Valider"
+
+				></td>
+		</tr>
+		<input type="hidden" name="idaeroport" value="
+		<?= ($laeroport !=null)? $laeroport['idaeroport'] : '' ?>">
+	</table>
+	
+</form>
 
